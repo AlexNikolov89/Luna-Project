4413b1d8c80c002ce8c92dd56e9000bd2a0bab51
lisc119 <mjchoo119@gmail.com>
2020-10-25 00:39:24 +0200

    setting urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,21 +1,26 @@
-"""app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+
+api_patterns = [
+    path('registration/', include('app.registration.urls')),
+    path('search/', include('app.search.urls')),
+    path('home/', include('app.home.urls')),
+    path('restaurants/', include('app.restaurants.urls')),
+    path('reviews/', include('app.reviews.urls')),
+    path('users/', include('app.users.urls')),
+
+    # path('docs/', include_docs_urls(title='Django Template', schema_url='/', permission_classes=[])),
+
+]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backend/admin/', admin.site.urls),
+    path('backend/api/', include(api_patterns)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
