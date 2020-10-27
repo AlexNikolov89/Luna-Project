ad68a3beca23321304138c5afaa161d7c25f95a6
Aleksandra Nikolov <nikolov.aleksandra@hotmail.com>
2020-10-27 11:34:02 +0100

    Homepage done

--- frontend/lunafrontend/src/components/homepage/style.js
+++ frontend/lunafrontend/src/components/homepage/style.js
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const HomepageContainer = styled.div `
 height: 100vh;
 width: 100%;
+`;
+
+export const BestRestaurantsContainer = styled.div `
+    
 `
