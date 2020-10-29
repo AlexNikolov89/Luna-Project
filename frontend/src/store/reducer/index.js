814c3523c35696ee9e9e5e7cca57a00bcc7c974d
stefan <381-samorsin@users.noreply.gitlab.propulsion-home.ch>
2020-10-29 22:22:23 +0100

    authReducer and authAction code added

--- frontend/src/store/reducer/index.js
+++ frontend/src/store/reducer/index.js
@@ -1,17 +1,26 @@
-import { GET_TOKEN } from '../actions/actionTypes'
+import { combineReducers } from 'redux';
+import { authReducer } from './authReducer'
 
-const initialState = {
-    token: "",
-}
+const reducers = combineReducers({
+    authReducer,
+});
+
+export default reducers
+
+// import { GET_TOKEN } from '../actions/actionTypes'
+
+// const initialState = {
+//     token: "",
+// }
 
 
-export const lunaReducer = (state = initialState, action) => {
+// export const lunaReducer = (state = initialState, action) => {
 
-    switch (action.type) {
-        case GET_TOKEN:
-            console.log("in da token ", action.payload)
-            return { ...state, token: action.payload }
-        default:
-            return state;
-    }
-}
+//     switch (action.type) {
+//         case GET_TOKEN:
+//             console.log("in da token ", action.payload)
+//             return { ...state, token: action.payload }
+//         default:
+//             return state;
+//     }
+// }
