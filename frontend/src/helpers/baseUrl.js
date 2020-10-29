3c4bb04778db884311050b69618fa5d89de8abe7
Aleksandra Nikolov <nikolov.aleksandra@hotmail.com>
2020-10-29 23:56:29 +0100

    commit

--- frontend/src/helpers/baseUrl.js
+++ frontend/src/helpers/baseUrl.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const baseUrl = 'https://taurus-luna.propulsion-learn.ch/'
-=======
 let baseUrl = 'https://taurus-luna.propulsion-learn.ch/';
->>>>>>> frontend_lina
 
 if(process.env.NODE_ENV === 'development'){
     baseUrl = 'http://localhost:8000'
