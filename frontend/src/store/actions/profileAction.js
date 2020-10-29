6cc89bb38fcc8c82aa54a8f5b78fdb33afe357e2
lisc119 <mjchoo119@gmail.com>
2020-10-29 22:42:44 +0100

    use baseUrl

--- frontend/src/store/actions/profileAction.js
+++ frontend/src/store/actions/profileAction.js
@@ -1,7 +1,8 @@
+import baseUrl from "../../helpers/baseUrl";
+
 export const getMyProfileAction = (dispatch, getState) => {
-    // const token = getState().auth.token;
-    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjA0MTc4MTkyLCJqdGkiOiJkMjNiM2M1ZWViYzQ0ZmRlOTYzYTMyZTFmMzY0NTc3NCIsInVzZXJfaWQiOjEyfQ.c2lG_vLaCk7c-Rwe0rCb47jVBo0rjZo5s6A3xg144YE"
-    fetch('http://0.0.0.0:8000/backend/api/users/me/', {
+    const token = getState().auth.token;
+    fetch(`${baseUrl}/backend/api/users/me/`, {
         headers: new Headers({
             Authorization: `Bearer ${token}`,
         }),
