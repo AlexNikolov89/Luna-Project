9b5609ccc999d7483cdd598d304d3a5739bbce92
stefan <381-samorsin@users.noreply.gitlab.propulsion-home.ch>
2020-10-30 13:32:29 +0100

    wtf

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Homepage from '../components/Homepage/Homepage.js';
+import Homepage from '../components/Homepage/homepage.js';
 import { Login } from '../components/LunaLogin';
 import { Registration } from '../components/Registration';
 import { Verification } from '../components/Verification';
