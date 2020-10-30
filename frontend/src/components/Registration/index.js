9a3db631fa6921630b29ebb5f61de08e0fb0b9aa
stefan <381-samorsin@users.noreply.gitlab.propulsion-home.ch>
2020-10-30 11:29:12 +0100

    footer fix 3

--- frontend/src/components/Registration/index.js
+++ frontend/src/components/Registration/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Style from './style';
 import { Navbar } from '../Header'
-import Footer from '../footer/footer.js';
+import Footer from '../footer';
 import baseUrl from '../../helpers/baseUrl'
 
 
